c7333e62ff9793ad324fa37ed6bb9227a8842fec
qoonmax <qoonmax@gmail.com>
2024-06-30 23:48:16 +0400

    first commit

---
+++ src/smallWidget.js
@@ -0,0 +1,146 @@
+// Stripe constants
+// Attention: It is not recommended to use Stripe secret keys.
+// Instead, it is better to use Restricted keys.
+// Create a new Restricted key in the Stripe dashboard, with read-only permissions for balance, customers, and disputes.
+const RESTRICTED_KEY = 'rk_test_51PX4JfRtTpNlld34vyfJTQ2t7Apt9HmHwRdSFQiO3WUNAQirJE3uWlOEqOxzlp4XptpAsphBSFrcdTOuU0fph3oD00i7FV3QH8';
+
+// Design constants
+const TITLE_FONT_SIZE = 12;
+const LARGE_VALUE_FONT_SIZE = 24;
+const VALUE_FONT_SIZE = 14;
+
+const w = await makeWidget();
+w.presentSmall();
+
+async function makeWidget() {
+    const widget = new ListWidget();
+
+    addBackgroundGradient(widget);
+
+    addStripeLabel(widget);
+
+    widget.addSpacer(20);
+
+    await addBalanceBlock(widget);
+
+    widget.addSpacer(20);
+
+    const stack = widget.addStack();
+    stack.layoutHorizontally();
+    stack.spacing = 20;
+
+    await addClientsBlock(stack);
+
+    await addRefundsBlock(stack);
+
+    return widget;
+}
+
+function addBackgroundGradient(widget) {
+    const gradient = new LinearGradient();
+    gradient.colors = [new Color("#635bff"), new Color("#1c1c1e")];
+    gradient.locations = [-1,1];
+    widget.backgroundGradient = gradient;
+}
+
+function addStripeLabel(widget) {
+    const stripeLabel = widget.addText("stripe");
+    stripeLabel.font = new Font("AvenirNext-Bold",14);
+}
+
+async function addBalanceBlock(widget) {
+    const balanceTitle = widget.addText("BALANCE");
+    balanceTitle.font = Font.mediumSystemFont(TITLE_FONT_SIZE);
+    balanceTitle.textColor = new Color("#ADBDCC");
+
+    const balanceStack = widget.addStack();
+    const result = await getBalance();
+    const balanceText = balanceStack.addText(result.currency + " " + result.amount.toString());
+    balanceText.font = Font.boldSystemFont(LARGE_VALUE_FONT_SIZE);
+}
+
+async function addClientsBlock(stack) {
+    const clientsStack = stack.addStack();
+    clientsStack.layoutVertically();
+
+    const clientsTitle = clientsStack.addText("CLIENTS");
+    clientsTitle.font = Font.mediumSystemFont(TITLE_FONT_SIZE);
+    clientsTitle.textColor = new Color("#ADBDCC");
+
+    const clientsValue = clientsStack.addText((await getCustomers()).toString());
+    clientsValue.font =  Font.boldSystemFont(VALUE_FONT_SIZE);
+}
+
+async function addRefundsBlock(stack) {
+    const refundsStack = stack.addStack();
+    refundsStack.layoutVertically();
+
+    const refundsTitle = refundsStack.addText("REFUNDS");
+    refundsTitle.font = Font.mediumSystemFont(TITLE_FONT_SIZE);
+    refundsTitle.textColor = new Color("#ADBDCC");
+
+    const refundsValue = refundsStack.addText((await getRefunds()).toString());
+    refundsValue.font =  Font.boldSystemFont(VALUE_FONT_SIZE);
+}
+
+async function getBalance() {
+    const mapCurrency = {
+        usd: "$",
+        eur: "€",
+        gbp: "£",
+        jpy: "¥",
+        aud: "A$",
+        cad: "C$",
+        chf: "CHF",
+        cny: "CN¥",
+        sek: "kr",
+        nzd: "NZ$",
+        rub: "₽",
+        hkd: "HK$",
+        nok: "kr",
+        krw: "₩",
+        try: "₺",
+        inr: "₹",
+        brl: "R$",
+        mxn: "MX$",
+        sgd: "S$",
+        idr: "Rp",
+        twd: "NT$",
+        thb: "฿",
+        php: "₱",
+    };
+
+    const url = 'https://api.stripe.com/v1/balance';
+    const request = new Request(url);
+    request.headers = {'Authorization': 'Bearer ' + RESTRICTED_KEY};
+
+    const json = await request.loadJSON();
+
+    const amount = (json.pending[0].amount ?? 0) / 100;
+    const currency = json.pending[0].currency ?? "usd";
+
+    return {
+        amount: amount,
+        currency: mapCurrency[currency]
+    };
+}
+
+async function getCustomers() {
+    const url = 'https://api.stripe.com/v1/customers';
+    const request = new Request(url);
+    request.headers = {'Authorization': 'Bearer ' + RESTRICTED_KEY};
+
+    const json = await request.loadJSON();
+
+    return json.data.length ?? 0;
+}
+
+async function getRefunds() {
+    const url = 'https://api.stripe.com/v1/refunds';
+    const request = new Request(url);
+    request.headers = {'Authorization': 'Bearer ' + RESTRICTED_KEY};
+
+    const json = await request.loadJSON();
+
+    return json.data.length ?? 0;
+}
